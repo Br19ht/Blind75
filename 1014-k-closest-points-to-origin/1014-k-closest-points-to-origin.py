1a548a03452d74c224ff5be977e1173d6c9d5441
Joshua Chang <65529656+Br19ht@users.noreply.github.com>
2023-06-14 16:19:53 -0400

    Time: 837 ms (41.63%), Space: 23.2 MB (22.41%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -6,7 +6,6 @@ class Solution:
             point.append((point[0] ** 2 + point[1] ** 2) ** 1/2)
 
         sortedPoints = sorted(points, key = lambda x: int(x[2]))
-        print(sortedPoints)
 
         for i in range(k):
             res.append([sortedPoints[i][0], sortedPoints[i][1]])
